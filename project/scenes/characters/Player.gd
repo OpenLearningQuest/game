abba213f3cbb94be5b09c7b633af4bec67ea380d
Brylie Christopher Oxley <brylie@protonmail.com>
2024-05-12 17:28:43 +0300

    Upgrade to Godot 4

--- project/scenes/characters/Player.gd
+++ project/scenes/characters/Player.gd
@@ -1,7 +1,7 @@
-extends KinematicBody2D
+extends CharacterBody2D
 
 # how fast the player should move in pixels per second
-export var player_speed = 100
+@export var player_speed = 100
 
 var speed_vector = Vector2(player_speed, player_speed)
 var move_to = null
@@ -11,32 +11,30 @@ func _input(event):
 		move_to = get_viewport().get_mouse_position()
 
 func _change_player_direction(velocity):
-	if velocity.x > velocity.y && velocity.x < -velocity.y:
+	if velocity.x > velocity.y and velocity.x < -velocity.y:
 		$PlayerAnimation.play("MoveUp")
-	elif velocity.x < velocity.y && velocity.x > -velocity.y:
+	elif velocity.x < velocity.y and velocity.x > -velocity.y:
 		$PlayerAnimation.play("MoveDown")
 	else:
 		$PlayerAnimation.play("MoveSide")
-		
 		$PlayerSprite.flip_h = velocity.x > 0
 
 func _physics_process(delta):
 	# only calculate move vector if player should move
 	if move_to:
 		var move_vector = move_to - global_position
-		
 		var player_arrived_at_destination = move_vector.length() < 3
 		
 		if player_arrived_at_destination:
 			# player should no longer move
 			move_to = null
 			
-			pass
+			# Stop moving by setting velocity to zero
+			velocity = Vector2.ZERO  
 		else:
 			# player should move
 			# normalize move vector to range 0:1
-			var velocity = move_and_slide(
-				move_vector.normalized() * speed_vector
-			)
+			velocity = move_vector.normalized() * speed_vector
+			move_and_slide()
 		
 			_change_player_direction(velocity)
